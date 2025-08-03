a4f777d08d87a95ea13009bfce41aeb2c626154a
Hashane <jhashane@gmail.com>
2025-08-03 19:38:57 +0300

    Bug-fix: Removed redundant entity greeting. Removed redundant setter for Id.

--- api/src/Entity/Customer.php
+++ api/src/Entity/Customer.php
@@ -63,13 +63,6 @@ class Customer
         return $this->id;
     }
 
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function getName(): ?string
     {
         return $this->name;
--- api/src/Entity/Reward.php
+++ api/src/Entity/Reward.php
@@ -42,12 +42,6 @@ class Reward
         return $this->id;
     }
 
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
-    }
     public function getTitle(): ?string
     {
         return $this->title;
